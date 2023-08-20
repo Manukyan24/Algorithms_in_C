78b83f539a11563d34ea51360b2dd6f053bd1005
Vladimir Manukyan <vladimir_manukyan@yahoo.com>
2023-08-20 11:08:28 +0400

    Added gitignore and upgraded  merge_sort

--- merge_sort.c
+++ merge_sort.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 
 
+/*
+
+		int* A is array that will be sorted
+		int p is lower border index of A from which array will be sorted 
+		int r is upper border index of A until which array will be sorted
+		int q is (p+r)/2
+
+		(In merge function, this indexes have little another role. Merge() function will be merge A[p...q] and A[q+1...r] sorted arrays in A )
+	
+
+*/
+
+
 
 void merge(int* A, int p, int q, int r){
 	int l_size = q-p+1;
@@ -16,33 +29,21 @@ void merge(int* A, int p, int q, int r){
 	for (i=0;i<r_size;i++){
 		R[i] = A[(q + 1) + i];
 	}
-	int j= 0, k = 0;
+	int j,k;
 	
-	
-	for (i = p; i < r+1; i++){
+	for (i=p, j = 0, k = 0; i < r+1; i++){
+			if (j == l_size){
+					A[i] = R[k++];
+					continue;  
+				}
+			if (k == r_size){
+					A[i] = L[j++];
+					continue;
+				}
+			A[i] = (L[j] < R[k]) ? L[j++] : R[k++];
+			}
 
-				if (L[j] <= R[k]){
-						if (j < l_size){
-							A[i] = L[j];
-							j++;
-						}
-						else if (k < r_size){
-								A[i] = R[k];
-								k++;
-							}
-					}
-				else{
-							if(k < r_size){
-							A[i] = R[k];
-							k++;
-							}
-							else if (j < l_size){
-									A[i] = L[j];
-									j++;
-								}
-					}
-		
-	}
+
 	
 	free(L);
 	free(R);
@@ -59,14 +60,13 @@ void merge_sort(int * A, int p, int r){
 
 
 int main(){
-	
-	int A[] = {1,104,68,20,0,1,2,3,-150};
+	// For testing
+	int A[] = {1,2,3,4,9,0,2,3,4,5,2};
 	int p = 0;
 	int r = sizeof(A)/sizeof(int) - 1;
-	int q = (p+r)/2;
-	//printf("%d \n\n\n\n", r);
 	merge_sort(A,p,r);
-	//merge(A,p,q,r);
+	
+	//Displaying array
 	for (p=0;p<r+1;p++){
 		printf("%d\n", A[p]);
 	}
